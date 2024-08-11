7feca64758f8e7ec07a5b2c6a38994ac6d4e818e
yoshi0729 <g021c1529@gmail.com>
2024-08-12 03:15:06 +0900

    Add files via upload

---
+++ 第2章/hello.py
@@ -0,0 +1,35 @@
+from flask import Flask
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Hello World</h1>'
+
+@app.route('/user')
+def user(name):
+    return '<h1>Hello,{}</h1>'.format(name)
+
+
+#$pip install flask
+'''
+$set flask_app=hello.py
+$set flask_debug=1
+$flask run
+'''
+
+'''
+$python
+>>>from hello import app
+>>>from flask import current_app
+>>>app_ctx=app.app_context()
+>>>app_ctx.push
+>>>current_app.name
+'hello'
+>>>app_ctx.pop()
+'''
+
+'''
+$python
+>>>from hello import app
+>>>app.url_map
+'''
